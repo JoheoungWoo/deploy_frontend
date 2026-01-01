a0e243d40daa06cedb2d1a7bd3001a1b98591115
JoheoungWoo <hataraci9350@daum.net>
2026-01-02 04:31:07 +0900

    대소문자 수정

--- src/router/admissionEducationRouter.jsx
+++ src/router/admissionEducationRouter.jsx
@@ -1,36 +1,58 @@
-import React, { lazy, Suspense } from 'react'
-import { Navigate } from 'react-router-dom';
-import BasicLayout from '../layouts/Basiclayout';
+import React, { lazy, Suspense } from "react";
+import { Navigate } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 const Loading = <div>Loading......</div>;
-const AdmissionGuide = lazy(() => import("../pages/admissionEducation/AdmissionGuidePage"));
+const AdmissionGuide = lazy(() =>
+  import("../pages/admissionEducation/AdmissionGuidePage")
+);
 const Colleges = lazy(() => import("../pages/admissionEducation/CollegesPage"));
-const GraduateSchool = lazy(() => import("../pages/admissionEducation/GraduateSchoolPage"));
-const NonDegreePrograms = lazy(() => import("../pages/admissionEducation/NonDegreeProgramsPage"));
+const GraduateSchool = lazy(() =>
+  import("../pages/admissionEducation/GraduateSchoolPage")
+);
+const NonDegreePrograms = lazy(() =>
+  import("../pages/admissionEducation/NonDegreeProgramsPage")
+);
 
 const admissionEducationRouter = () => {
   return [
     {
-        path:"",
-        element:<Navigate replace to="admissionguide" />   
-    }, 
-    {
-        path:"admissionguide",
-        element:<Suspense fallback={Loading}><BasicLayout children={<AdmissionGuide />}/></Suspense>
+      path: "",
+      element: <Navigate replace to="admissionguide" />,
     },
     {
-        path:"colleges",
-        element:<Suspense fallback={Loading}><BasicLayout children={<Colleges />}/></Suspense>
+      path: "admissionguide",
+      element: (
+        <Suspense fallback={Loading}>
+          <BasicLayout children={<AdmissionGuide />} />
+        </Suspense>
+      ),
     },
     {
-        path:"graduateschool",
-        element:<Suspense fallback={Loading}><BasicLayout children={<GraduateSchool />}/></Suspense>
+      path: "colleges",
+      element: (
+        <Suspense fallback={Loading}>
+          <BasicLayout children={<Colleges />} />
+        </Suspense>
+      ),
     },
     {
-        path:"nondegreeprograms",
-        element:<Suspense fallback={Loading}><BasicLayout children={<NonDegreePrograms />}/></Suspense>
+      path: "graduateschool",
+      element: (
+        <Suspense fallback={Loading}>
+          <BasicLayout children={<GraduateSchool />} />
+        </Suspense>
+      ),
     },
-  ]
-}
+    {
+      path: "nondegreeprograms",
+      element: (
+        <Suspense fallback={Loading}>
+          <BasicLayout children={<NonDegreePrograms />} />
+        </Suspense>
+      ),
+    },
+  ];
+};
 
-export default admissionEducationRouter
+export default admissionEducationRouter;
